ca9a658951ad7959319f66c329bb77dc31e09170
Ryan Kelley <rdkelley@users.noreply.github.com>
2020-10-14 10:43:57 -0700

    Remove random h1 in sensors.js

--- web/client/src/content/Dashboard/sensors.js
+++ web/client/src/content/Dashboard/sensors.js
@@ -38,7 +38,6 @@ function Sensors() {
       {/* Chart */}
       <div className="bx--row sensors-page__r2">
         <div className="bx--col-lg-16">
-            <h1>THIS THING FIVE</h1>
           <AccelerationsChart />
         </div>
       </div>
